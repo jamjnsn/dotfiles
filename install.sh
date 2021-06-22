2f12dd758cacf2de5b8908289498572a4d206d60
Jamie Jansen <jamie@jnsn.me>
2021-06-21 22:08:43 -0700

    Remove Homebrew from install

--- install.sh
+++ install.sh
@@ -11,20 +11,5 @@ ln -sf $DOTFILES/zsh/zshrc $ZDOTDIR/.zshrc
 ln -sf $DOTFILES/nanorc $HOME/.nanorc
 ln -sf $DOTFILES/inputrc $HOME/.inputrc
 
-# Install Homebrew
-if ! command -v brew &> /dev/null
-then
-    echo "Installing Homebrew..."
-    git clone https://github.com/Homebrew/brew ~/.linuxbrew/Homebrew
-    mkdir -p $HOMEBREW/bin
-    ln -s $HOMEBREW/Homebrew/bin/brew $HOMEBREW/bin
-fi
-
-# Install Homebrew packages
-brew install zsh
-
-brew tap cantino/mcfly
-brew install mcfly
-
 # Reload shell
 exec zsh

d574afd32f3a8b6560d41ba1099696bd0f4f9d84
Jamie Jansen <jamie@jnsn.me>
2021-06-21 19:44:14 -0700

    Improve install script, add Homebrew

--- install.sh
+++ install.sh
@@ -1,8 +1,25 @@
 #!/bin/zsh
 
-ln -sf $PWD/zsh/zshenv $HOME/.zshenv
-source $HOME/.zshenv
+source ./zsh/zshenv
 
+# Install zsh
+mkdir -p $ZDOTDIR;
+ln -sf $DOTFILES/zsh/zshenv $HOME/.zshenv
 ln -sf $DOTFILES/zsh/zshrc $ZDOTDIR/.zshrc
-ln -sf $DOTFILES/nanorc $ZDOTDIR/.nanorc
-ln -sf $DOTFILES/inputrc $ZDOTDIR/.inputrc
+
+# Install other dotfiles
+ln -sf $DOTFILES/nanorc $HOME/.nanorc
+ln -sf $DOTFILES/inputrc $HOME/.inputrc
+
+# Install Homebrew
+if ! command -v brew &> /dev/null
+then
+    echo "Installing Homebrew..."
+    git clone https://github.com/Homebrew/brew ~/.linuxbrew/Homebrew
+    mkdir ~/.linuxbrew/bin
+    ln -s ~/.linuxbrew/Homebrew/bin/brew ~/.linuxbrew/bin
+fi
+
+# Install Homebrew packages
+brew tap cantino/mcfly
+brew install mcfly
